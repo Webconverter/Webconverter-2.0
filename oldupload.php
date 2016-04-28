f6f59f1f2d03a4e201f3b989fd4b2d7e18a4d257
Haakon <haakon@underbakke.net>
2016-04-28 10:10:08 +0200

    dobbel erlik yo

--- oldupload.php
+++ oldupload.php
@@ -81,13 +81,13 @@ if ($uploadOk == 0) {
 $im = new Imagick($target_file);
 $img_format = null;
 $wished_format = $fto;
-if($img_format = "jpg"){
- if($wished_format = "png"){
+if($img_format == "jpg"){
+ if($wished_format == "png"){
  $im->setimageformat( "png" );
   }
 }
-else if ($img_format = "png"){
-  if($wished_format = "jpg"){
+else if ($img_format == "png"){
+  if($wished_format == "jpg"){
     $im->setimageformat(" jpg ");
   }
 }
