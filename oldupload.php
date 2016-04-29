313dffef3bbe7d52cb04f480e2e7b4b7b61e8bdf
Vetle Lode <vetlelode@gmail.com>
2016-04-29 01:16:18 -0700

    zxc

--- oldupload.php
+++ oldupload.php
@@ -76,7 +76,7 @@ if ($uploadOk == 0) {
 
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_REQUEST["fileToUpload"]["tmp_name"], $target_file)) {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
